e12c08e0114acc8dd488a6838626f93b77c8373a
MaherDissem <maherdissem26@gmail.com>
2023-11-03 13:00:23 -0400

    fix and made location sseperate

--- src/data/collect_aemo_data.py
+++ src/data/collect_aemo_data.py
@@ -1,11 +1,14 @@
+import os
 import subprocess
 
 save_folder = "dataset/raw/AEMO"
 
-for year in range(2000, 2021+1): 
-    for month in ['01','02','03','04','05','06','07','08','09','10','11','12']:
-        for location in ["NSW", "QLD", "VIC", "SA", "TAS"]:
-            bash_command = f"curl https://aemo.com.au/aemo/data/nem/priceanddemand/PRICE_AND_DEMAND_{year}06_NSW1.csv > {save_folder}/{year}{month}{location}.csv"
+for location in ["NSW", "QLD", "VIC", "SA", "TAS"]:
+    for year in range(2017, 2022+1): 
+        for month in ['01','02','03','04','05','06','07','08','09','10','11','12']:
+            save_path = f"{save_folder}/{location}/{year}{month}.csv"
+            os.makedirs(os.path.dirname(save_path), exist_ok=True)
+            bash_command = f"curl https://aemo.com.au/aemo/data/nem/priceanddemand/PRICE_AND_DEMAND_{year}{month}_{location}1.csv > {save_path}.csv"
             result = subprocess.run(bash_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
             if result.returncode != 0:
                 print("Command failed:")
