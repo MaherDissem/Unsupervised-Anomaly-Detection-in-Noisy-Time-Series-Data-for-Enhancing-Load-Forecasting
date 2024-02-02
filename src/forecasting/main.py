72c1eab99ecc4042ee984cf834529abbe6eed776
MaherDissem <maherdissem26@gmail.com>
2024-02-02 17:55:23 -0500

    removed extra params

--- src/forecasting/main.py
+++ src/forecasting/main.py
@@ -19,57 +19,48 @@ from src.utils.utils import set_seed
 def parse_args():
     parser = argparse.ArgumentParser(description="Runs Load Forecasting experiments")
     # dataset
-    parser.add_argument("--train_dataset_path",   type=str,       default="dataset/processed/AEMO/NSW/exp0/lf_contam",       help="Path to train dataset")
-    parser.add_argument("--test_dataset_path",    type=str,       default="dataset/processed/AEMO/NSW/exp0/lf_cleaned",    help="Path to clean dataset for testing")
+    parser.add_argument("--train_dataset_path",     type=str,       default="dataset/processed/AEMO/NSW/exp0/lf_contam",       help="Path to train dataset")
+    parser.add_argument("--test_dataset_path",      type=str,       default="dataset/processed/AEMO/NSW/exp0/lf_cleaned",    help="Path to clean dataset for testing")
     # sequence
-    parser.add_argument("--timesteps",            type=int,       default=48*6,     help="Number of timesteps")
-    parser.add_argument("--sequence_split",       type=float,     default=5/6,      help="Ratio of input to target (forecasting horizon) split")
-    parser.add_argument("--nbr_var",              type=int,       default=1,        help="Number of variables")
+    parser.add_argument("--timesteps",              type=int,       default=48*6,     help="Number of timesteps")
+    parser.add_argument("--sequence_split",         type=float,     default=5/6,      help="Ratio of input to target (forecasting horizon) split")
+    parser.add_argument("--nbr_var",                type=int,       default=1,        help="Number of variables")
     # training
-    parser.add_argument("--epochs",               type=int,       default=500,      help="Number of epochs")
-    parser.add_argument("--patience",             type=int,       default=50,       help="Patience for early stopping")
-    parser.add_argument("--batch_size",           type=int,       default=32,       help="Batch size")
-    parser.add_argument("--lr",                   type=float,     default=1e-3,     help="Learning rate")
-    parser.add_argument("--seed",                 type=int,       default=0)
-    parser.add_argument("--checkpoint_path",      type=str,       default="src/forecasting/checkpoint.pt",       help="Path to save checkpoint")
-    parser.add_argument("--verbose",              type=bool,      default=True,     help="Verbosity")
-    parser.add_argument("--eval_every",            type=int,       default=10,       help="Evaluate every n epochs")
+    parser.add_argument("--epochs",                 type=int,       default=500,      help="Number of epochs")
+    parser.add_argument("--patience",               type=int,       default=50,       help="Patience for early stopping")
+    parser.add_argument("--batch_size",             type=int,       default=32,       help="Batch size")
+    parser.add_argument("--lr",                     type=float,     default=1e-3,     help="Learning rate")
+    parser.add_argument("--seed",                   type=int,       default=0)
+    parser.add_argument("--checkpoint_path",        type=str,       default="src/forecasting/checkpoint.pt",       help="Path to save checkpoint")
+    parser.add_argument("--verbose",                type=bool,      default=True,     help="Verbosity")
+    parser.add_argument("--eval_every",             type=int,       default=10,       help="Evaluate every n epochs")
     # visualization
-    parser.add_argument("--n_plots",              type=int,       default=32,                                    help="Number of plots")
-    parser.add_argument("--save_plots_path",      type=str,       default="results/forecasting/contam",          help="Path to save plots")
-    parser.add_argument("--results_file",         type=str,       default="results/results.txt",                 help="Path to file to save results in")
-    # model selection
-    parser.add_argument("--model_choice",         type=str,       default="scinet", help="Model to use for forecasting: seq2seq or scinet")
+    parser.add_argument("--n_plots",                type=int,       default=32,                                    help="Number of plots")
+    parser.add_argument("--save_plots_path",        type=str,       default="results/forecasting/contam",          help="Path to save plots")
+    parser.add_argument("--results_file",           type=str,       default="results/results.txt",                 help="Path to file to save results in")
+    # model selection  
+    parser.add_argument("--model_choice",           type=str,       default="scinet", help="Model to use for forecasting: seq2seq or scinet")
     # seq2seq2 model parameters, only relevant if model == "seq2seq"
-    parser.add_argument("--hidden_size",          type=int,       default=128,      help="Hidden size of the model")
-    parser.add_argument("--num_grulstm_layers",   type=int,       default=1,        help="Number of GRU/LSTM layers")
-    parser.add_argument("--fc_units",             type=int,       default=16,       help="Number of fully connected units") 
+    parser.add_argument("--hidden_size",            type=int,       default=128,      help="Hidden size of the model")
+    parser.add_argument("--num_grulstm_layers",     type=int,       default=1,        help="Number of GRU/LSTM layers")
+    parser.add_argument("--fc_units",               type=int,       default=16,       help="Number of fully connected units") 
     # SCINet model parameters, only relevant if model == "scinet"
-    ### -------  training settings --------------  
-    parser.add_argument('--save', type=str, default='model/model.pt', help='path to save the final model')
-    parser.add_argument('--optim', type=str, default='adam')
-    parser.add_argument('--L1Loss', type=bool, default=True)
-    parser.add_argument('--weight_decay',type=float,default=0.00001,help='weight decay rate')
-    parser.add_argument('--lradj', type=int, default=2,help='adjust learning rate')
-    parser.add_argument('--save_path', type=str, default='exp/financial_checkpoints/')
-    parser.add_argument('--model_name', type=str, default='SCINet')
-    ### -------  model settings --------------  
-    parser.add_argument('--concat_len', type=int, default=0)
-    parser.add_argument('--hidden-size', default=1.0, type=float, help='hidden channel of module')# H, EXPANSION RATE
-    parser.add_argument('--INN', default=1, type=int, help='use INN or basic strategy')
-    parser.add_argument('--kernel', default=5, type=int, help='kernel size')#k kernel size
-    parser.add_argument('--dilation', default=1, type=int, help='dilation')
-    parser.add_argument('--positionalEcoding', type = bool , default=False)
-    parser.add_argument('--dropout', type=float, default=0.25)
-    parser.add_argument('--groups', type=int, default=1)
-    parser.add_argument('--levels', type=int, default=3) # 3 for 24*5 and 48*5, 48*7*2, 24*7*2 input length
-    parser.add_argument('--num_decoder_layer', type=int, default=1)
-    parser.add_argument('--stacks', type=int, default=1)
-    parser.add_argument('--long_term_forecast', action='store_true', default=False)
-    parser.add_argument('--RIN', type=bool, default=False)
-    parser.add_argument('--decompose', type=bool,default=False)
-    parser.add_argument('--single_step', type=int, default=0, help='only supervise the final setp')
-    parser.add_argument('--single_step_output_One', type=int, default=0, help='only output the single final step')
+    parser.add_argument('--L1Loss',                 type=bool,      default=True)
+    parser.add_argument('--lradj',                  type=int,       default=2,        help='adjust learning rate')
+    parser.add_argument('--concat_len',             type=int,       default=0)
+    parser.add_argument('--hidden-size',            default=1.0,    type=float,       help='hidden channel of module')
+    parser.add_argument('--kernel',                 default=5,      type=int,         help='kernel size')
+    parser.add_argument('--positionalEcoding',      type=bool,      default=False)
+    parser.add_argument('--dropout',                type=float,     default=0.25)
+    parser.add_argument('--groups',                 type=int,       default=1)
+    parser.add_argument('--levels',                 type=int,       default=3)
+    parser.add_argument('--num_decoder_layer',      type=int,       default=1)
+    parser.add_argument('--stacks',                 type=int,       default=1) # TODO compare 1 vs 2
+    parser.add_argument('--long_term_forecast',     action='store_true',     default=False) # TODO compare T vs F
+    parser.add_argument('--RIN',                    type=bool,      default=False)
+    parser.add_argument('--decompose',              type=bool,      default=False)
+    parser.add_argument('--single_step',            type=int,       default=0,        help='only supervise the final setp')     # disabled (need to change loss function to use)
+    parser.add_argument('--single_step_output_One', type=int,       default=0,        help='only output the single final step') # disabled
 
     args = parser.parse_args()
     if not args.long_term_forecast:
