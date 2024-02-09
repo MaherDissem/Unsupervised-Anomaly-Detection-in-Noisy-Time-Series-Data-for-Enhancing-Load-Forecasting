456a34ad275ac28de4837816dca78357b024f641
MaherDissem <maherdissem26@gmail.com>
2024-02-09 14:02:47 -0500

    improved synth logic

--- src/data_processing/synth_anomaly.py
+++ src/data_processing/synth_anomaly.py
@@ -3,6 +3,23 @@ import numpy as np
 
 class SynthLoadAnomaly():
 
+    def __init__(
+            self, 
+            prob_1=0.25, prob_2=0.25, prob_3=0.25, prob_4=0.25, prob_softstart=0.5, prob_extreme=0.5, anomaly_max_length=12, 
+            seed=0
+        ):
+        
+        self.prob_1 = prob_1
+        self.prob_2 = prob_2
+        self.prob_3 = prob_3
+        self.prob_4 = prob_4
+        self.prob_softstart = prob_softstart
+        self.prob_extreme = prob_extreme
+        self.anomaly_max_length = anomaly_max_length
+        self.seed = seed
+        np.random.seed(seed)
+
+
     def _anomaly_type1(self, target, indices, lengths, k=0):
         """
         Anomaly type 1 that drops the power time series values to a negative value potentially followed by zero values
@@ -79,36 +96,47 @@ class SynthLoadAnomaly():
         return target
 
 
-    def inject_anomaly(self, sequence, anom_type=0, n_anom=1):
+    def _inject_anomaly(self, sequence, anom_type=1):
         sequence = sequence.copy()
         n = len(sequence)
         
-        if anom_type==0:
-            anom_type = np.random.randint(1, 4)
-
         if anom_type==1:
             position = np.random.randint(n//4, (len(sequence)-1)//2)
-            length = np.random.randint(3, 8)
+            length = np.random.randint(3, min(n-position-1, self.anomaly_max_length))
             anom_idx = range(position, position+length)
             anomalous_sequence = self._anomaly_type1(sequence.copy(), [position], [length])
             return anomalous_sequence, anom_idx
         
-        if anom_type==2:
+        elif anom_type==2:
             position = np.random.randint(n//4, (len(sequence)-1)//2)
-            length = np.random.randint(2, 8)
+            length = np.random.randint(2, min(n-position-1, self.anomaly_max_length))
             anom_idx = range(position, position+length)
-            anomalous_sequence = self._anomaly_type2(sequence.copy(), [position], [length])
+            softstart = np.random.choice([True, False], p=[self.prob_softstart, 1-self.prob_softstart])
+            anomalous_sequence = self._anomaly_type2(sequence.copy(), [position], [length], softstart)
             return anomalous_sequence, anom_idx
         
-        if anom_type==3:
-            position = np.random.randint(n//4, (len(sequence)-1)//3*2)
+        elif anom_type==3:
+            position = np.random.randint(n//4, (len(sequence)-1)//4*3)
             anom_idx = range(position, position+1)
-            anomalous_sequence = self._anomaly_type3(sequence.copy(), [position], [1])
+            is_extreme = np.random.choice([True, False], p=[self.prob_extreme, 1-self.prob_extreme])
+            anomalous_sequence = self._anomaly_type3(sequence.copy(), [position], [1], is_extreme)
             return anomalous_sequence, anom_idx
         
-        if anom_type==4:
-            position = np.random.randint(n//4, (len(sequence)-1)//3*2)
+        elif anom_type==4:
+            position = np.random.randint(n//4, (len(sequence)-1)//4*3)
             anom_idx = range(position, position+1)
-            anomalous_sequence = self._anomaly_type4(sequence.copy(), [position], [1])
+            is_extreme = np.random.choice([True, False], p=[self.prob_extreme, 1-self.prob_extreme])
+            anomalous_sequence = self._anomaly_type4(sequence.copy(), [position], [1], is_extreme)
             return anomalous_sequence, anom_idx
-        
+
+        else:
+            raise NotImplementedError("Anomaly type not implemented")
+
+
+    def inject_anomaly(self, sequence, n_anomalies=1):
+        anom_types = np.random.choice([1, 2, 3, 4], n_anomalies, p=[self.prob_1, self.prob_2, self.prob_3, self.prob_4])
+        anom_indices = []
+        for i in range(n_anomalies):
+            sequence, anom_idx = self._inject_anomaly(sequence, anom_types[i])
+            anom_indices.extend(anom_idx)
+        return sequence, anom_indices
